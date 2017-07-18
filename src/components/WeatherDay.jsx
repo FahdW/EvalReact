11add400dd47844bf45a54778cd85c81da151ab2
Fahd Wahab <fahd.jwahab@gmail.com>
2017-07-18 10:54:34 -0400

    fix for viewing

--- src/components/WeatherDay.jsx
+++ src/components/WeatherDay.jsx
@@ -9,8 +9,8 @@ export default class WeatherDay extends Component {
       <Col className="weather-container" xs={2} lg={1}>
         <p className="text-center weather-day">{day}</p>
         <img className="center-block weather-img" alt="sun" src={Sun} />
-        <p className="text-center weather-high">High: {high}</p>
-        <p className="text-center weather-low">Low: {low}</p>
+        <p className="text-center weather-high">High:{high}</p>
+        <p className="text-center weather-low">Low:{low}</p>
       </Col>
     );
   }
